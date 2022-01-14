706eb9df480ca3cc81fefb7294add1dd0734ecf0
Phillips Okere <philipsonokere@gmail.com>
2022-01-14 13:51:28 +0100

    Update message.php

--- message.php
+++ message.php
@@ -1,1 +1,19 @@
 
+<?php
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$phone= $_POST['phone'];
+$message= $_POST['message'];
+$to = "philipsonokere@gmail.com";
+$subject = "Mail From my Website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email .
+"\r\n  Phone = " . $phone .
+"\r\n Message =" . $message;
+$headers = "From: nemobug.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+
+?>
